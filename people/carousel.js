90a25d47d7c335d14bf6923368d3d520d55750bf
TheNinza <niks.a3198@gmail.com>
2022-09-06 17:54:22 +0530

    added swiper implementation

--- people/carousel.js
+++ people/carousel.js
@@ -4,119 +4,48 @@
  * @description: This file contains implementation for the carousel.
  */
 
+// images
+const imagesToAdd = [
+  "/assets/people/2.webp",
+  "/assets/people/3.webp",
+  "/assets/people/4.webp",
+  "/assets/people/5.webp",
+];
+
 // variables
-class Carousel {
-  constructor() {
-    this.initVariables();
-    this.initElements();
-    this.updateClasses();
-    this.initEventListeners();
-  }
 
-  initVariables() {
-    this.carouselContainer = document.querySelector(".carousel-container");
-    this.carouselBackground = document.querySelector(
-      ".carousel-background-overlay"
-    );
-    this.carouselControlLeft = document.querySelector(".carousel-control-left");
-    this.carouselControlRight = document.querySelector(
-      ".carousel-control-right"
-    );
+const overlayImage = document.querySelector(".carousel-background-overlay");
+const alreadyPresentImageSrc = overlayImage.src;
+const imagesContainer = document.querySelector(".carousel-image-container");
 
-    this.images = [
-      "/assets/people/2.webp",
-      "/assets/people/3.webp",
-      "/assets/people/4.webp",
-      "/assets/people/5.webp",
-    ];
+const overlays = [alreadyPresentImageSrc, ...imagesToAdd];
 
-    this.totalImages = this.images.length + 1;
+// add images to the carousel
+imagesToAdd.forEach((image) => {
+  const wrapperElement = document.createElement("div");
+  wrapperElement.classList.add("swiper-slide");
 
-    this.currentImageIndex = 0;
-    this.leftImageIndex = this.getActualImageIndex(this.currentImageIndex - 1);
-    this.rightImageIndex = this.getActualImageIndex(this.currentImageIndex + 1);
-  }
+  const imageElement = document.createElement("img");
+  imageElement.src = image;
+  imageElement.classList.add("carousel-image");
+  imageElement.setAttribute("alt", "carousel-image");
+  imageElement.setAttribute("height", "400");
+  imageElement.setAttribute("width", "400");
 
-  initElements() {
-    this.images.forEach((image) => {
-      const imageContainer = document.createElement("div");
-      imageContainer.classList.add("carousel-image-container");
-      imageContainer.classList.add("hidden");
+  wrapperElement.appendChild(imageElement);
+  imagesContainer.appendChild(wrapperElement);
+});
 
-      const imageElement = document.createElement("img");
-      imageElement.classList.add("carousel-image");
-      imageElement.setAttribute("src", image);
-      imageElement.setAttribute("alt", "carousel-image");
+// initialize swiper
+const swiper = new Swiper(".swiper", {
+  navigation: {
+    nextEl: ".carousel-control-right",
+    prevEl: ".carousel-control-left",
+  },
+  loop: true,
+});
 
-      imageContainer.appendChild(imageElement);
-      this.carouselContainer.appendChild(imageContainer);
-    });
-
-    this.carouselImages = document.querySelectorAll(
-      ".carousel-image-container"
-    );
-
-    this.images = [
-      this.carouselImages[0].querySelector("img").src,
-      ...this.images,
-    ];
-  }
-
-  updateClasses(currIdx = 0) {
-    this.currentImageIndex = currIdx;
-    this.leftImageIndex = this.getActualImageIndex(this.currentImageIndex - 1);
-    this.rightImageIndex = this.getActualImageIndex(this.currentImageIndex + 1);
-    this.carouselBackground.src = this.images[this.currentImageIndex];
-
-    this.carouselImages.forEach((image, index) => {
-      if (index === this.leftImageIndex) {
-        image.classList.add("left");
-        image.classList.remove("right");
-        image.classList.remove("hidden");
-      } else if (index === this.rightImageIndex) {
-        image.classList.add("right");
-        image.classList.remove("left");
-        image.classList.remove("hidden");
-      } else if (index === this.currentImageIndex) {
-        image.classList.remove("left");
-        image.classList.remove("right");
-        image.classList.remove("hidden");
-      } else {
-        image.classList.remove("left");
-        image.classList.remove("right");
-        image.classList.add("hidden");
-      }
-    });
-  }
-
-  initEventListeners() {
-    this.carouselControlLeft.addEventListener(
-      "click",
-      this.onLeftClick.bind(this)
-    );
-    this.carouselControlRight.addEventListener(
-      "click",
-      this.onRightClick.bind(this)
-    );
-  }
-
-  onLeftClick() {
-    this.updateClasses(this.leftImageIndex);
-  }
-
-  onRightClick() {
-    this.updateClasses(this.rightImageIndex);
-  }
-
-  getActualImageIndex(index) {
-    if (index < 0) {
-      return this.totalImages - 1;
-    } else if (index >= this.totalImages) {
-      return 0;
-    } else {
-      return index;
-    }
-  }
-}
-
-const carousel = new Carousel();
+// change overlay image
+swiper.on("slideChange", () => {
+  overlayImage.src = overlays[swiper.realIndex];
+});
